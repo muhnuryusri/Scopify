69e7fc262fa5dc619973f3eebaefd3b718ed5dfd
muhnuryusri <muhnuryuusri@gmail.com>
2023-06-10 13:01:15 +0800

    update entity (domain)

--- core/src/main/java/com/example/scopify/domain/entity/Category.kt
+++ core/src/main/java/com/example/scopify/domain/entity/Category.kt
@@ -1,5 +1,6 @@
 package com.example.scopify.domain.entity
 
 data class Category(
+    val id: String,
     val category: String
 )
--- core/src/main/java/com/example/scopify/domain/entity/Source.kt
+++ core/src/main/java/com/example/scopify/domain/entity/Source.kt
@@ -1,6 +1,6 @@
 package com.example.scopify.domain.entity
 
 data class Source(
-    val id: String,
-    val name: String,
+    val id: String?,
+    val name: String?,
 )
